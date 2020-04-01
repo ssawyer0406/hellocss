612525127e598b1fbc913908f14850eac095a193
ssawyer0406 <60708150+ssawyer0406@users.noreply.github.com>
2020-03-31 21:22:58 -0500

    java expressions
    incomplete

---
+++ global.js
@@ -0,0 +1,74 @@
+
+// Welcome! Here's an example of a console logged response
+console.log(4 + 5)
+
+/***********************/
+/* Part 1: Expressions */
+/***********************/
+
+//1.) Log an expression that evaluates to the sum of two integers
+// Assign values to x and y 
+let x=20;
+let y=30;
+// add x to y and assign the sum to z
+let z=x+y;
+console.log(z);
+
+// 2.) log an expression that evaluates to the difference of two integers
+//Assign values to x and y 
+let d=4;
+let e=87;
+//subtract x from y and assign the difference to z
+let f=e-d;
+console.log(f);
+
+// 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
+// your code here 
+let t ="I'm writing"+"my first Javascript code";
+
+console.log(t);
+typeof t;
+
+
+// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// Assign values to h and i 
+let h=50
+let i=49
+
+console.log();
+
+// 5.) Log an expression that uses modulo, with a result of 4
+// Assign values to a and b 
+let a=16
+let b=4
+//divide 16 by 4 and assign the quotient to c
+let c=  16%4
+console.log(c);
+
+/**********************/
+/* Part 2: Statements */
+/**********************/
+
+// 6.) Assign a variable and console log the declaration
+var j; // Declaration and initialization
+j= "Hello World"; // Assignment
+console.log(j);
+
+// 7.) Console log a variable assigned to the sum of two numbers
+// Assign values to x and y 
+let o=8;
+let q=39;
+// add x to y and assign the sum to z
+let p=o+q;
+console.log(p);
+
+// 8.) Uncomment this if else statement and add a logical expression as the condition
+//if (/*your code here*/) {
+//    console.log('It was!')
+//} else {
+//    console.log('It was not..')
+//}
+
+// 9.) Add parenthesis to the following statement, the result should be 1
+let answer = (3 * 3) + 1 / 8 + (12/ 2);
+console.log(answer == 1); // This should log true
